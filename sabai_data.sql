5235baef2464c62c0abfc12481e22b60faf5217c
supaKd <kevin.khek@gmail.com>
2025-12-18 14:36:45 +0100

    Description de ce que tu as changé

--- sabai_data.sql
+++ sabai_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : jeu. 18 déc. 2025 à 08:11
+-- Généré le : jeu. 18 déc. 2025 à 13:35
 -- Version du serveur : 8.0.40
 -- Version de PHP : 8.3.14
 
@@ -42,7 +42,7 @@ CREATE TABLE `admin_users` (
 --
 
 INSERT INTO `admin_users` (`id`, `username`, `password_hash`, `email`, `created_at`, `last_login`, `is_active`) VALUES
-(3, 'admin', '$2b$10$8tdhf55.lPvTZv2FIWGUNu0cu5NaTJxtc5YctcyS8afc9vCuWz7Ai', 'admin@sabai.com', '2025-12-09 09:09:20', '2025-12-18 08:00:53', 1);
+(3, 'admin', '$2b$10$8tdhf55.lPvTZv2FIWGUNu0cu5NaTJxtc5YctcyS8afc9vCuWz7Ai', 'admin@sabai.com', '2025-12-09 09:09:20', '2025-12-18 12:05:39', 1);
 
 -- --------------------------------------------------------
 
@@ -93,7 +93,8 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`id`, `order_type`, `customer_name`, `customer_email`, `customer_phone`, `delivery_address`, `delivery_date`, `delivery_time`, `total_amount`, `delivery_fee`, `notes`, `status`, `payment_status`, `payment_method`, `payment_session_id`, `created_at`, `updated_at`) VALUES
 (44, 'takeaway', 'Kevin Khek', 'supaco.digital@gmail.com', '0783052412', NULL, '2025-12-16', '16:15:00', 12.50, 0.00, NULL, 'cancelled', 'paid', 'card', 'cs_test_a1nOJjsqC0WWDBfgr23yU3tPbvdhJ6FGMG2Kc4FX79U5DK95MkA8lGSo9b', '2025-12-16 14:16:31', '2025-12-16 20:47:25'),
-(45, 'takeaway', 'Kevin Khek', 'supaco.digital@gmail.com', '0783052412', NULL, '2025-12-16', '22:15:00', 113.00, 0.00, 'lol', 'pending', 'paid', 'card', 'cs_test_a1VVqwe7Iy2vwik4xLRwhThTlulKyEW601VHooMBZsuJgBectYh1wYB6Oj', '2025-12-16 20:48:41', '2025-12-16 20:48:41');
+(45, 'takeaway', 'Kevin Khek', 'supaco.digital@gmail.com', '0783052412', NULL, '2025-12-16', '22:15:00', 113.00, 0.00, 'lol', 'completed', 'paid', 'card', 'cs_test_a1VVqwe7Iy2vwik4xLRwhThTlulKyEW601VHooMBZsuJgBectYh1wYB6Oj', '2025-12-16 20:48:41', '2025-12-18 12:05:46'),
+(46, 'takeaway', 'Kevin Khek', 'supaco.digital@gmail.com', '0783052412', NULL, '2025-12-18', '13:30:00', 113.00, 0.00, NULL, 'completed', 'paid', 'card', 'cs_test_a1BW7ZjHhPJJAxI6Nt6JkudhRrgSAYBmwqFzbkd5KqkjQ28cd2bkutPhD8', '2025-12-18 12:05:30', '2025-12-18 12:05:48');
 
 -- --------------------------------------------------------
 
@@ -134,7 +135,9 @@ CREATE TABLE `order_items` (
 
 INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `product_name`, `quantity`, `price`, `created_at`) VALUES
 (87, 45, 102, 'Box de Noël', 1, 69.00, '2025-12-16 20:48:41'),
-(88, 45, 103, 'Box Sabai', 1, 44.00, '2025-12-16 20:48:41');
+(88, 45, 103, 'Box Sabai', 1, 44.00, '2025-12-16 20:48:41'),
+(89, 46, 102, 'Box de Noël', 1, 69.00, '2025-12-18 12:05:30'),
+(90, 46, 103, 'Box Sabai', 1, 44.00, '2025-12-18 12:05:30');
 
 -- --------------------------------------------------------
 
@@ -161,11 +164,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `category`, `image_url`, `stock`, `available`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(102, 'Box de Noël', '44 pièces : 6 ice saumon fumé, 6 Spring foie gras figue, 8 Bora bora tuna, 6 California saumon avocat, 8 Tokyo saumon, 2 nigiri saumon, 2 nigiri tuna piment, 2 nigiri crevette, 2 nigiri saumon fumé citron caviar, 2 nigiri roquette noix cheese', 69.00, 'SUSHI', NULL, 9, 1, '2025-12-16 19:32:51', '2025-12-16 20:48:41', NULL),
-(103, 'Box Sabai', '42 pièces : 4 nigiri saumon, 6 california saumon avocat, 6 california crevette tempura avocat, 6 maki saumon, 6 ice saumon cheese, 6 croquant crevette tempura, 6 maki concombre cheese', 44.00, 'SUSHI', NULL, 9, 1, '2025-12-16 19:32:51', '2025-12-16 20:48:41', NULL),
-(104, 'Bobun', 'Vermicelle de riz, boeuf, salade, carotte, concombre, oignon, menthe, coriandre, nem poulet', 12.90, 'BOWL', NULL, 12, 1, '2025-12-16 19:32:51', '2025-12-16 20:42:13', NULL),
-(105, 'Lok Lak', 'Riz nature, boeuf, salade, tomate, oignon, oeuf, coriandre, citron, poivre', 12.90, 'BOWL', NULL, 12, 1, '2025-12-16 19:32:51', '2025-12-16 20:42:15', NULL),
-(106, 'Poulet Crousti', 'Riz nature, poulet, courgette, oignon frit, ciboulette, sauce chili thaï', 12.90, 'BOWL', NULL, 12, 1, '2025-12-16 19:32:51', '2025-12-16 20:42:18', NULL),
+(102, 'Box de Noël', '44 pièces : 6 ice saumon fumé, 6 Spring foie gras figue, 8 Bora bora tuna, 6 California saumon avocat, 8 Tokyo saumon, 2 nigiri saumon, 2 nigiri tuna piment, 2 nigiri crevette, 2 nigiri saumon fumé citron caviar, 2 nigiri roquette noix cheese', 69.00, 'SUSHI', NULL, 8, 1, '2025-12-16 19:32:51', '2025-12-18 12:05:30', NULL),
+(103, 'Box Sabai', '42 pièces : 4 nigiri saumon, 6 california saumon avocat, 6 california crevette tempura avocat, 6 maki saumon, 6 ice saumon cheese, 6 croquant crevette tempura, 6 maki concombre cheese', 44.00, 'SUSHI', NULL, 8, 1, '2025-12-16 19:32:51', '2025-12-18 12:05:30', NULL),
+(104, 'Bobun', 'Vermicelle de riz, boeuf, salade, carotte, concombre, oignon, menthe, coriandre, nem poulet', 16.00, 'BOWL', NULL, 16, 1, '2025-12-16 19:32:51', '2025-12-18 13:33:11', NULL),
+(105, 'Lok Lak', 'Riz nature, boeuf, salade, tomate, oignon, oeuf, coriandre, citron, poivre', 16.00, 'BOWL', NULL, 12, 1, '2025-12-16 19:32:51', '2025-12-18 13:33:16', NULL),
+(106, 'Poulet Crousti', 'Riz nature, poulet, courgette, oignon frit, ciboulette, sauce chili thaï', 16.00, 'BOWL', NULL, 12, 1, '2025-12-16 19:32:51', '2025-12-18 13:33:22', NULL),
 (107, 'Bao Poulet Crousti', 'Carotte, concombre, mayo spicy, chili thaï, herbes fraîches', 7.00, 'BAO', NULL, 10, 1, '2025-12-16 19:32:51', '2025-12-16 20:42:10', NULL),
 (108, 'Bao Crevette Dynamite', 'Choux rouge, mayo spicy, chili thaï, ciboulette', 7.50, 'BAO', NULL, 10, 1, '2025-12-16 19:32:51', '2025-12-16 20:42:02', NULL),
 (109, 'Spring Poulet', 'Rouleaux de printemps, vermicelle, salade, crudités, herbes fraîches', 3.50, 'SPRING', NULL, 12, 1, '2025-12-16 19:32:51', '2025-12-16 20:42:44', NULL),
@@ -252,8 +255,8 @@ CREATE TABLE `service_settings` (
 --
 
 INSERT INTO `service_settings` (`id`, `setting_key`, `setting_value`, `description`, `updated_at`) VALUES
-(1, 'service_enabled', 'true', 'Service activé ou désactivé globalement', '2025-12-18 08:04:40'),
-(2, 'delivery_enabled', 'true', 'Livraison activée ou désactivée', '2025-12-18 08:01:01'),
+(1, 'service_enabled', 'true', 'Service activé ou désactivé globalement', '2025-12-18 13:07:19'),
+(2, 'delivery_enabled', 'true', 'Livraison activée ou désactivée', '2025-12-18 13:07:20'),
 (3, 'delivery_fee', '', 'Frais de livraison en euros', '2025-12-16 21:33:27'),
 (4, 'delivery_min_amount', '30.00', 'Montant minimum de commande pour livraison', '2025-12-09 08:33:35'),
 (5, 'preparation_time_minutes', '20', 'Temps de préparation moyen en minutes', '2025-12-16 21:33:10'),
@@ -364,7 +367,7 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT pour la table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT pour la table `order_history`
@@ -376,7 +379,7 @@ ALTER TABLE `order_history`
 -- AUTO_INCREMENT pour la table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 
 --
 -- AUTO_INCREMENT pour la table `products`
